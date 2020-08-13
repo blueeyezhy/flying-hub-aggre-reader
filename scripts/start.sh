a01c2ed5b4e78d66ee43f9ac1bf7849ec7790ed1
blueeyezhy <blueeye_zhy@outlook.com>
2020-08-13 19:11:14 +0800

    modify to conbine seaky.club

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,5 +2,5 @@ docker login -u prs-os -p pressone dockerhub.qingcloud.com
 docker pull dockerhub.qingcloud.com/pressone/atom
 docker pull dockerhub.qingcloud.com/pressone/aggre_reader
 
-docker-compose  -f docker-compose.yml down --remove-orphans && 
+docker-compose -f docker-compose.yml  down --remove-orphans && 
 docker-compose up -d
--- scripts/start_prod.sh
+++ scripts/start_prod.sh
@@ -1,6 +1,6 @@
 docker login -u prs-os -p pressone dockerhub.qingcloud.com
-docker pull dockerhub.qingcloud.com/pressone/aggre_reader
 docker pull dockerhub.qingcloud.com/pressone/atom
+docker pull dockerhub.qingcloud.com/pressone/aggre_reader
 
 docker-compose -f docker-compose.yml  down --remove-orphans &&
 docker-compose -f docker-compose.yml  up -d
